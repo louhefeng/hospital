d198e823c0dc8083d2da2e0bda056a531022ad3e
louhefeng <louhefeng@ecareyun.com>
2018-01-09 18:59:57 +0800

    数据库和设计

--- hospitalprivate.sql
+++ hospitalprivate.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50720
 File Encoding         : 65001
 
-Date: 2018-01-09 18:10:19
+Date: 2018-01-09 18:59:29
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -35,3 +35,46 @@ CREATE TABLE `user` (
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES ('6', '1', '18231131593', '1', '2017-12-26 11:34:26', '1', '1', '1');
+
+-- ----------------------------
+-- Table structure for `userlog`
+-- ----------------------------
+DROP TABLE IF EXISTS `userlog`;
+CREATE TABLE `userlog` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `vipid` bigint(20) NOT NULL,
+  `type` int(11) NOT NULL COMMENT '0卡消费1现金消费2充值3修改',
+  `yiliao` varchar(255) DEFAULT NULL,
+  `money` bigint(20) NOT NULL,
+  `createdate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `tezheng` varchar(255) DEFAULT NULL,
+  `oldusermoney` varchar(255) NOT NULL COMMENT '操作前会员余额',
+  `updatedate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of userlog
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `vipuser`
+-- ----------------------------
+DROP TABLE IF EXISTS `vipuser`;
+CREATE TABLE `vipuser` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(22) NOT NULL,
+  `phone` varchar(11) NOT NULL,
+  `addr` varchar(100) NOT NULL,
+  `money` bigint(20) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `imgurl` varchar(255) DEFAULT NULL,
+  `sex` int(11) DEFAULT NULL,
+  `creatdate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `updatedate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of vipuser
+-- ----------------------------
